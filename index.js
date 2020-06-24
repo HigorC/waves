1dad096649629a203755c5c8e97aa41b59a36081
Higor Chaves <higor_oliveira@outlook.com>
2020-06-24 16:13:11 -0400

    Refactoring

--- index.js
+++ index.js
@@ -16,86 +16,43 @@ function getElement(actualIndex) {
 function getLeftElement(actualIndex) {
     const leftIndex = actualIndex - 1;
     if (leftIndex % tableWidth === tableWidth - 1) return null
-    return {
-        index: leftIndex,
-        element: getElement(leftIndex)
-    }
+    return getElement(leftIndex)
 }
 
 function getRigthElement(actualIndex) {
     const rigthIndex = actualIndex + 1;
     if (rigthIndex % tableWidth === 0) return null
-    return {
-        index: rigthIndex,
-        element: getElement(rigthIndex)
-    }
+    return getElement(rigthIndex)
 }
 
 function getTopElement(actualIndex) {
     const topIndex = actualIndex - tableWidth;
     if (topIndex < 0) return null
-    return {
-        index: topIndex,
-        element: getElement(topIndex)
-    }
+    return getElement(topIndex)
 }
 
 function getDownElement(actualIndex) {
     const downIndex = actualIndex + tableWidth;
     if (downIndex > tableWidth * tableHeigth) return null
-    return {
-        index: downIndex,
-        element: getElement(downIndex)
-    }
+    return getElement(downIndex)
 }
 
 function createWave(element) {
-    function wavePropagation(element, actualPower, waveInfo, history) {
+    function wavePropagation(element, actualPower, history) {
         //Recursion stop
         if (!element || actualPower === 0) return;
+        if (history.has(element.dataset.position)) return;
 
         const actualIndex = Number(element.dataset.position);
-
-        if (history.has(element.dataset.position)) return;
 
         element.innerHTML = actualPower
         history.add(element.dataset.position)
 
         setTimeout(() => {
-            const topElement = getTopElement(actualIndex)
-
-            if (topElement && topElement.index < waveInfo.sourceIndex) {
-                wavePropagation(topElement.element, actualPower - 1, {
-                    sourceIndex: waveInfo.sourceIndex
-                }, history)
-            }
-
-            const leftElement = getLeftElement(actualIndex)
-
-            if (leftElement && leftElement.index < waveInfo.sourceIndex) {
-                wavePropagation(leftElement.element, actualPower - 1, {
-                    sourceIndex: waveInfo.sourceIndex
-                }, history)
-            }
-
-            const downElement = getDownElement(actualIndex)
-
-            if (downElement && downElement.index > waveInfo.sourceIndex) {
-                wavePropagation(downElement.element, actualPower - 1, {
-                    sourceIndex: waveInfo.sourceIndex
-                }, history)
-            }
-
-            const rigthElement = getRigthElement(actualIndex)
-
-            if (rigthElement && rigthElement.index > waveInfo.sourceIndex) {
-                wavePropagation(rigthElement.element, actualPower - 1, {
-                    sourceIndex: waveInfo.sourceIndex
-                }, history)
-            }
-
-
-
+            wavePropagation(getTopElement(actualIndex), actualPower - 1, history)
+            wavePropagation(getLeftElement(actualIndex), actualPower - 1, history)
+            wavePropagation(getDownElement(actualIndex), actualPower - 1, history)
+            wavePropagation(getRigthElement(actualIndex), actualPower - 1, history)
 
             element.innerHTML = ''
         }, 1000);
@@ -103,8 +60,7 @@ function createWave(element) {
 
     const history = new Set()
 
-    wavePropagation(element, maxPower, { sourceIndex: element.dataset.position }, history)
-
+    wavePropagation(element, maxPower, history)
 }
 
 function inicializeTable() {
