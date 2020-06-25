af51d46cebf72878f618dc62586e8cf8ed428761
Higor Chaves <higor_oliveira@outlook.com>
2020-06-25 11:03:39 -0400

    Create footer, general fixes

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const tableWidth = 10;
-const tableHeigth = 10;
+const tableSize = 10;
+const totalSize = tableSize * tableSize
+
+const delayPropagation = 50
 
 const posicoes = []
 
-const maxPower = 10
+const colors = [
+    "#1e0492",
+    "#550fcb",
+    "#e07df3",
+    "#6a6582",
+    "#c4b6cc",
+    "#ADFF02",
+    "#34343a",
+    "#01BEFE",
+    "#FFDD00",
+    "#FF7D00",
+    "#FF006D",
+    "#FF006D",
+    "#ADFF02",
+    "#8F00FF"
+]
 
-for (let i = 0; i < tableWidth * tableHeigth; i++) {
+const getRandonColor = () => {
+    return colors[Math.floor(Math.random() * colors.length - 1)]
+}
+
+const maxPower = colors.length - 1
+
+for (let i = 0; i < totalSize; i++) {
     posicoes.push(i);
 }
 
@@ -15,52 +38,56 @@ function getElement(actualIndex) {
 
 function getLeftElement(actualIndex) {
     const leftIndex = actualIndex - 1;
-    if (leftIndex % tableWidth === tableWidth - 1) return null
+    if (leftIndex % tableSize === tableSize - 1) return null
     return getElement(leftIndex)
 }
 
 function getRigthElement(actualIndex) {
     const rigthIndex = actualIndex + 1;
-    if (rigthIndex % tableWidth === 0) return null
+    if (rigthIndex % tableSize === 0) return null
     return getElement(rigthIndex)
 }
 
 function getTopElement(actualIndex) {
-    const topIndex = actualIndex - tableWidth;
+    const topIndex = actualIndex - tableSize;
     if (topIndex < 0) return null
     return getElement(topIndex)
 }
 
 function getDownElement(actualIndex) {
-    const downIndex = actualIndex + tableWidth;
-    if (downIndex > tableWidth * tableHeigth) return null
+    const downIndex = actualIndex + tableSize;
+    if (downIndex > totalSize) return null
     return getElement(downIndex)
 }
 
 function createWave(element) {
-    function wavePropagation(element, actualPower, history) {
+    function wavePropagation(element, actualPower, history, color) {
         //Recursion stop
-        if (!element || actualPower === 0) return;
+        if (!element) return;
         if (history.has(element.dataset.position)) return;
 
         const actualIndex = Number(element.dataset.position);
 
-        element.innerHTML = actualPower
+        // element.innerHTML = actualPower
+        // element.bgColor = getColor(actualPower)
+        element.bgColor = color
         history.add(element.dataset.position)
 
         setTimeout(() => {
-            wavePropagation(getTopElement(actualIndex), actualPower - 1, history)
-            wavePropagation(getLeftElement(actualIndex), actualPower - 1, history)
-            wavePropagation(getDownElement(actualIndex), actualPower - 1, history)
-            wavePropagation(getRigthElement(actualIndex), actualPower - 1, history)
+            wavePropagation(getTopElement(actualIndex), actualPower - 1, history, color)
+            wavePropagation(getLeftElement(actualIndex), actualPower - 1, history, color)
+            wavePropagation(getDownElement(actualIndex), actualPower - 1, history, color)
+            wavePropagation(getRigthElement(actualIndex), actualPower - 1, history, color)
 
-            element.innerHTML = ''
-        }, 1000);
+            setTimeout(() => {
+                element.bgColor = '#DCECC9'
+            }, delayPropagation)
+
+        }, delayPropagation);
     }
 
     const history = new Set()
-
-    wavePropagation(element, maxPower, history)
+    wavePropagation(element, maxPower, history, getRandonColor())
 }
 
 function inicializeTable() {
@@ -68,9 +95,9 @@ function inicializeTable() {
 
     let tableContent = '<table>';
 
-    for (let i = 0, count = 0; i < tableWidth; i++) {
+    for (let i = 0, count = 0; i < tableSize; i++) {
         tableContent += '<tr>'
-        for (let j = 0; j < tableWidth; j++ , count++) {
+        for (let j = 0; j < tableSize; j++ , count++) {
             tableContent += `<td data-position=${count} onclick="createWave(this)"></td>`
         }
         tableContent += '</tr>'
