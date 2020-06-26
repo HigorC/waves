0a7c6c73f8ff8ff16471e5b724d13134ebfbc438
Higor Chaves <higor_oliveira@outlook.com>
2020-06-26 11:30:21 -0400

    JS docs

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const totalSize = tableSize * tableSize
 
 const delayPropagation = 50
 
-const posicoes = []
-
 const colors = [
     "#1e0492",
     "#550fcb",
@@ -22,45 +20,84 @@ const colors = [
     "#8F00FF"
 ]
 
+const maxPower = colors.length - 1
+
+/**
+ * Returns a randon color
+ * @returns { String }
+ */
 const getRandonColor = () => {
     return colors[Math.floor(Math.random() * colors.length - 1)]
 }
 
-const maxPower = colors.length - 1
-
-for (let i = 0; i < totalSize; i++) {
-    posicoes.push(i);
-}
-
+/**
+ * Return a td with the passed index
+ * @param { Number } actualIndex 
+ * @returns { HTMLElement }
+ */
 function getElement(actualIndex) {
     return document.querySelector(`[data-position="${actualIndex}"]`)
 }
 
+/**
+ * Check if exists a left td element and return it
+ * @param { Number } actualIndex 
+ * @returns { HTMLElement }
+ */
 function getLeftElement(actualIndex) {
     const leftIndex = actualIndex - 1;
     if (leftIndex % tableSize === tableSize - 1) return null
     return getElement(leftIndex)
 }
 
+/**
+ * Check if exists a rigth td element and return it
+ * @param { Number } actualIndex 
+ * @returns { HTMLElement }
+ */
 function getRigthElement(actualIndex) {
     const rigthIndex = actualIndex + 1;
     if (rigthIndex % tableSize === 0) return null
     return getElement(rigthIndex)
 }
 
+/**
+ * Check if exists a top td element and return it
+ * @param { Number } actualIndex 
+ * @returns { HTMLElement }
+ */
 function getTopElement(actualIndex) {
     const topIndex = actualIndex - tableSize;
     if (topIndex < 0) return null
     return getElement(topIndex)
 }
 
+/**
+ * Check if exists a down td element and return it
+ * @param { Number } actualIndex 
+ * @returns { HTMLElement }
+ */
 function getDownElement(actualIndex) {
     const downIndex = actualIndex + tableSize;
     if (downIndex > totalSize) return null
     return getElement(downIndex)
 }
 
+/**
+ * Use the passed element as the source of wave and call the propagation function
+ * @param { HTMLElement } element 
+ * @returns { Void }
+ */
 function createWave(element) {
+    /**
+     * For each passed element, call's recursivly four times, one for each direction
+     * The recursion stops when the element don't exists or if the element already was propagated
+     * @param { HTMLElement } element 
+     * @param { Number } actualPower 
+     * @param { Set } history 
+     * @param { String } color 
+     * @returns { Void }
+    */
     function wavePropagation(element, actualPower, history, color) {
         //Recursion stop
         if (!element) return;
@@ -90,6 +127,10 @@ function createWave(element) {
     wavePropagation(element, maxPower, history, getRandonColor())
 }
 
+/**
+ * Create intire table, with the positions to can be possible find each element
+ * @returns { Void }
+ */
 function inicializeTable() {
     const table = document.querySelector('table');
 
